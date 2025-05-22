f797bd75a25092d0f63bfa1df4b3a58bdfb216f3
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-05-22 14:41:56 +0700

    Update edge.bat

--- edge.bat
+++ edge.bat
@@ -1,3 +1,3 @@
 @echo off
-irm https://go.bibica.net/edge | iex
+powershell -Command "Invoke-WebRequest 'https://go.bibica.net/edge' -OutFile 'edge.ps1'; powershell -ExecutionPolicy Bypass -File 'edge.ps1'"
 pause
